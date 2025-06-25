357a29e7a2613210a28726681d4d052e72499048
rootzombies <td254580@gmail.com>
2025-06-25 21:27:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from colorama import Fore, init
+from pyfiglet import figlet_format
+import os
+os.system("clear")
+os.system("cls")
+init(autoreset=True)
+print(Fore.GREEN + figlet_format("WannaHappy", font="slant"))
+print(Fore.CYAN + "WannaHappy: Ransomware Blocker and Decryptor")
+print(Fore.RESET)
+print("""
+      [1]:  Ransomware RunTime Blocker
+      [2]:  Ransomware Encrypted File Decryptor
+      [3]:  Scan a Suspicious File  """)
+choice = int(input(">> "))
+if choice == 1:
+    print("Thx to those who use this tool")
+    print("\n")
+    os.system("python blocker.py")
+elif choice == 2:
+    print("Thx to those who use this tool")
+    print("\n")
+    os.system("python WannaHappy.py")
+elif choice == 3:
+    print("Thx to those who use this tool")
+    print("\n")
+    os.system("python scan.py")
